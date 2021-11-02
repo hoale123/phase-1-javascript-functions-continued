c566b5d409fe85d32e638971494a0ebbb0df23f7
Hoa Le <Hoa_le9485@yahoo.com>
2021-11-01 21:51:53 -0400

    commit tree e41ed14385e13dd0b5b08a0042007323ac0f1511

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Your code here
+function saturdayFun(fun = "roller-skate") // default parameter
+ {
+    return `This Saturday, I want to ${fun}!`
+}
+
+//function expression const = 
+const mondayWork = function (work = "go to the office") {
+    return `This Monday, I will ${work}.`
+}
+
+function wrapAdjective(style= "*") {
+   return function (word = "special") {
+        return `You are ${style}${word}${style}!`
+    }
+}
+// wrapAdjective("%")("a dedicated programmer");
+// //=> "You are %a dedicated programmer%!"
